cc475f4d1593d2031fbd39cce4a569ceedcc2662
1dontkillme <abldtesttt@gmail.com>
2024-09-20 22:33:47 +0300

    ads

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    assetPrefix: process.env.NODE_ENV === 'production' ? '.' : '.',
-    basePath: process.env.NODE_ENV === 'production' ? '.' : '.',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '/',
+    basePath: process.env.NODE_ENV === 'production' ? '/' : '/',
     trailingSlash: true,
     output: 'export',
 };
